b5895ed699804b168f6eda125ca26331102ad3f1
dennereed <denne.reed@gmail.com>
2021-08-10 08:07:00 -0700

    custom email registration templates

--- blackinbioanth/settings/production.py
+++ blackinbioanth/settings/production.py
@@ -19,7 +19,7 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.cssmin.CSSMinFilter',
 ]
 
-ALLOWED_HOSTS = [env("DJANGO_ALLOWED_HOST_NAME"), ]
+ALLOWED_HOSTS = [env("DJANGO_ALLOWED_HOST_NAME"), env("DJANGO_PRODUCTION_IP")]
 
 DATABASES['default'] = env.db('PROD_DATABASE_URL')
 
